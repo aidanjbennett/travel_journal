8646c3215be0c3ba1449a6f29aa6932c9b020574
Aidan Bennett <57372629+aidanjbennett@users.noreply.github.com>
2026-02-10 23:11:27 +0000

    Use app name var instead

--- lib/shared/widgets/main_title_widget.dart
+++ lib/shared/widgets/main_title_widget.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:travel_journal/constants.dart';
 
 class MainTitleWidget extends StatelessWidget {
   const MainTitleWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(8.0),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
       child: Text(
-        "Travel Journal",
+        appName,
         textAlign: TextAlign.center,
-        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
       ),
     );
   }
