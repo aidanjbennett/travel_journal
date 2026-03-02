f4869f521a72a1afee6429ee516ec570a01b4bd7
Aidan Bennett <57372629+aidanjbennett@users.noreply.github.com>
2026-03-02 10:26:31 +0000

    Change package to my own domain

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.travel_journal"
+    namespace = "com.aidanjbennett.travel_journal_flutter"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.travel_journal"
+        applicationId = "com.aidanjbennett.travel_journal_flutter"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/aidanjbennett/travel_journal_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/aidanjbennett/travel_journal_flutter/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.travel_journal
+package com.aidanjbennett.travel_journal_flutter
 
 import io.flutter.embedding.android.FlutterActivity
 
