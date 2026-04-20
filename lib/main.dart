0804add1f249d6969f48988ed9f0c8222b99e2d2
Aidan Bennett <57372629+aidanjbennett@users.noreply.github.com>
2026-04-20 21:10:00 +0100

    Update imports

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:travel_journal/database/app_database.dart';
 import 'package:travel_journal/screens/entries_screen.dart';
-import 'package:travel_journal/model/entries_view_model.dart';
+import 'package:travel_journal/providers/entries_view_model.dart';
 import 'package:travel_journal/screens/home_screen.dart';
-import 'package:travel_journal/model/home_view_model.dart';
+import 'package:travel_journal/providers/home_view_model.dart';
 import 'package:travel_journal/providers/journal_provider.dart';
 import 'package:travel_journal/screens/setting_screen.dart';
 
